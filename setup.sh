f57b893b61e49ea0aeae6a0d3ca3f9dec55d2958
Reguide Author <hagilawork@gmail.com>
2024-11-05 10:47:49 +0700

    update

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ git clone https://github.com/ReJumpLabs/cysic-verifier.git
 curl -L https://github.com/cysic-labs/phase2_libs/releases/download/v1.0.0/verifier_linux > ~/cysic-verifier/verifier
 curl -L https://github.com/cysic-labs/phase2_libs/releases/download/v1.0.0/libdarwin_verifier.so > ~/cysic-verifier/libdarwin_verifier.so
 
-cd ~/cysic-verifier/
+cd /root/cysic-verifier/
 
 echo 'Type evm wallet addresses'
 
@@ -18,3 +18,30 @@ echo 'Start config file instance docker'
 sleep 3
 
 python3 config.py
+
+echo 'Start config docker-compose'
+
+output_file="docker-compose.yml"
+
+echo "version: '3'" > $output_file
+echo "services:" >> $output_file
+
+i=1
+while IFS= read -r evm_address || [ -n "$evm_address" ]; do
+  cat <<EOL >> $output_file
+  verifier_instance_$i:
+    build: .
+    environment:
+      - CHAIN_ID=534352
+    volumes:
+      - ./data/cysic/keys:/root/.cysic/keys
+      - ./data/scroll_prover:/root/.scroll_prover
+    network_mode: "host"
+    restart: unless-stopped
+    command: ["$evm_address"]
+
+EOL
+  i=$((i + 1))
+done < evm.txt
+
+echo "docker-compose.yml generated with $((i - 1)) instances."
