680340e5e29864eab23b388d60843f92071c1610
Standinwol <toantrum9x@gmail.com>
2025-06-05 17:29:04 +0700

    Update README.sh

--- README.sh
+++ README.sh
@@ -1,1 +1,3 @@
-rm -f setup.sh && wget https://raw.githubusercontent.com/ReJumpLabs/cysic-verifier/refs/heads/master/setup.sh -O setup.sh && chmod +x setup.sh && ./setup.sh
+```bash
+rm -f setup.sh && wget https://raw.githubusercontent.com/Standinwol/cysic-verifier/refs/heads/master/setup.sh -O setup.sh && chmod +x setup.sh && ./setup.sh
+```
